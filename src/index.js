97aebb3e0048bcdad92fd57fd0112489fc2b9cf2
yangel20 <61720403+yangel20@users.noreply.github.com>
2021-02-16 20:20:02 -0500

    fix button to be pixle perfect black, added a rotating colors for image Animation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles/navbar.scss"
 import "./styles/body.scss"
 import "./styles/image.scss"
 import "./styles/canvas.scss"
+//  ^^^^ causes a Uncaught SyntaxError: Cannot use import statement outside a module
 
 
 
